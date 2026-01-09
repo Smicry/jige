368c1da19e89b5d3525e912fd628c085e3c0297d
dravengan <dravengan@tencent.com>
2026-01-09 15:05:59 +0800

    use py 3.13

---
+++ agents/agents.py
@@ -0,0 +1,7 @@
+from deepagents import CompiledSubAgent, SubAgent
+
+
+def get_agents() -> list[CompiledSubAgent | SubAgent]:
+    agents = []
+    print(f"agents 个数 {len(agents)}")
+    return agents
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from agents.agents import get_agents
+
+
 def main():
-    print("Hello from jige!")
+    while True:
+        try:
+            i = input("🤖 你好，我能为你做些什么？\n")
+            print(f"收到请求：{i}")
+            match i:
+                case "quit" | "exit":
+                    break
+                case _:
+                    get_agents()
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == "__main__":
